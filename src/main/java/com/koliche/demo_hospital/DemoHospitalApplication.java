c2b713dd5f3edaebbcbb38a6580742832be25823
koliche <koliche.moahmed0@gmail.com>
2023-07-29 12:57:49 +0100

    First Commit

---
+++ src/main/java/com/koliche/demo_hospital/DemoHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.koliche.demo_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHospitalApplication.class, args);
+    }
+
+}
