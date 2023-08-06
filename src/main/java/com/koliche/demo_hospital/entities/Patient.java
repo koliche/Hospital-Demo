907349567d937941b2890ded69ba20e76445dacd
koliche <koliche.moahmed0@gmail.com>
2023-08-06 14:33:14 +0100

    add edit patient and new patient

--- src/main/java/com/koliche/demo_hospital/entities/Patient.java
+++ src/main/java/com/koliche/demo_hospital/entities/Patient.java
@@ -1,9 +1,9 @@
 package com.koliche.demo_hospital.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,13 +16,16 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank
+    @Size(min = 4, max = 20)
     private String name;
 
+    @Temporal(TemporalType.DATE)
     private Date berthDay;
 
     private boolean sick;
 
+    @Min(50)
     private int score;
 
 
--- src/main/java/com/koliche/demo_hospital/web/PatientController.java
+++ src/main/java/com/koliche/demo_hospital/web/PatientController.java
@@ -2,16 +2,15 @@ package com.koliche.demo_hospital.web;
 
 import com.koliche.demo_hospital.entities.Patient;
 import com.koliche.demo_hospital.repository.PatientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class PatientController {
@@ -30,15 +29,37 @@ public class PatientController {
         model.addAttribute("pages", new int[pagePatient.getTotalPages()]);
         model.addAttribute("currentPage", page);
         model.addAttribute("keyword", keyword);
-        return "Patient";
+        return "patient";
     }
 
     @GetMapping("/delete")
-    public String delete(Long id, String keyword, int page){
+    public String delete(Long id,
+                         @RequestParam(defaultValue = "") String keyword,
+                         @RequestParam(defaultValue = "0") int page){
         patientRepository.deleteById(id);
         return "redirect:/index?page=%d&keyword=%s".formatted(page, keyword);
     }
 
+    @GetMapping("/formPatient")
+    public String formPatient(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatient";
+    }
+
+    @PostMapping("/savePatient")
+    public String savePatient(@Valid Patient patient, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {return "formPatient";}
+
+        patientRepository.save(patient);
+        return "redirect:/index?keyword=%s".formatted(patient.getName());
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model, @RequestParam Long id){
+        Patient patient = patientRepository.findById(id).get();
+        model.addAttribute("patient", patient);
+        return "editPatient";
+    }
 
 
 
